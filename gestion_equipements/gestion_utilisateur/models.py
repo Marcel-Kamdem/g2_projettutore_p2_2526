e2b38602aa0f1393a5116772ccec667ddca84a82
Jonh-jonhy <jonathankamdem000@gmail.com>
2026-03-18 21:29:40 +0100

    modification de model : ajout des modes utilisateurs, gestionnaire, administrateur

--- gestion_equipements/gestion_utilisateur/models.py
+++ gestion_equipements/gestion_utilisateur/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+# Modele utilisateur ~ motheer
+class Utilisateur(AbstractUser):
+    #Les attributs ID, et Mot de passes sonts geres automatiquement par django
+    nom = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    ROLE_CHOICES = [('ADMIN','Administrateur'),('GEST','Gestionnaire')]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    #informations pour le login
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'nom']
+
+    def __str__(self):
+        return f"{self.nom} - {self.role} "
+
+
+
+class Administrateur(Utilisateur):
+    class Meta:
+        verbose_name = "Administrateur"
+
+
+class Gestionnaire(Utilisateur):
+    class Meta:
+        verbose_name = "Gestionnaire"
+        verbose_name_plural = "Gestionnaires"
